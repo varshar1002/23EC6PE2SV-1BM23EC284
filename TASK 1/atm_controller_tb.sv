9f338070ed2a917a9dc5dec0849a5f756a22af2b
varshar1002 <varshar.ec23@bmsce.ac.in>
2026-02-10 11:39:17 +0530

    Update atm_controller_tb.sv

--- TASK 1/atm_controller_tb.sv
+++ TASK 1/atm_controller_tb.sv
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 // File       : atm_controller_tb.sv
-// Author     : Prajwal Bharadwaj D H
+// Author     : Varsha R
 // Created    : 2026-02-03
 // Module     : tb
 // Project    : SystemVerilog and Verification (23EC6PE2SV)
