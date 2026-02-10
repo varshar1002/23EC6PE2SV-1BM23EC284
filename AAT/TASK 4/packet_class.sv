a6d03cf5fcb38e344d7c4e790b0e8833cfbcfc36
varshar1002 <varshar.ec23@bmsce.ac.in>
2026-02-10 10:16:36 +0530

    Update packet_class.sv

--- AAT/TASK 4/packet_class.sv
+++ AAT/TASK 4/packet_class.sv
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // File        : packet_class.sv
-// Author      : Prajwal Bharadwaj D H (1BM23EC186)
+// Author      : Varsha R (1BM23EC284)
 // Created     : 2026-01-29
 // Module      : packet_class
 // Project     : SystemVerilog and Verification (23EC6PE2SV),
