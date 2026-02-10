dd880b3c82fb21981c1952eac756443eb99243c3
varshar1002 <varshar.ec23@bmsce.ac.in>
2026-02-10 10:15:49 +0530

    Update atm_controller.sv

--- AAT/TASK 3/atm_controller.sv
+++ AAT/TASK 3/atm_controller.sv
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // File        : atm_controller.sv
-// Author      : Prajwal Bharadwaj D H (1BM23EC186)
+// Author      : Varsha R (1BM23EC284)
 // Created     : 2026-01-29
 // Module      : atm_controller
 // Project     : SystemVerilog and Verification (23EC6PE2SV),
