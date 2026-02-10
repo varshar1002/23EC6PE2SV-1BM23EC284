691470049e513fea69b1050191768e3ad4564aa2
varshar1002 <varshar.ec23@bmsce.ac.in>
2026-02-10 10:14:55 +0530

    Update digital_clock_tb.sv

--- AAT/TASK 2/digital_clock_tb.sv
+++ AAT/TASK 2/digital_clock_tb.sv
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // File        : digital_clock.sv
-// Author      : Prajwal Bharadwaj D H (1BM23EC186)
+// Author      : Varsha R (1BM23EC284)
 // Created     : 2026-01-28
 // Module      : counter
 // Project     : SystemVerilog and Verification (23EC6PE2SV),
