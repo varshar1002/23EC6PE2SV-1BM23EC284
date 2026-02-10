35ac50f91d3423cc95f4542ae5c2820a68599609
varshar1002 <varshar.ec23@bmsce.ac.in>
2026-02-10 11:39:45 +0530

    dual_port_ram.sv

--- TASK 2/dual_port_ram.sv
+++ TASK 2/dual_port_ram.sv
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // File        : dual_port_ram.sv
-// Author      : Prajwal Bharadwaj D H (1BM23EC186)
+// Author      : Varsha R
 // Created     : 2026-02-03
 // Module      : dual_port_ram
 // Project     : SystemVerilog and Verification (23EC6PE2SV),
