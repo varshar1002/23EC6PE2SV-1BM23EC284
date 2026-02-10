2d6d20e605bce9c76d3f44ed71b6df038403486d
varshar1002 <varshar.ec23@bmsce.ac.in>
2026-02-10 09:46:10 +0530

    Update dual_port_ram_tb.sv

--- AAT (CADENCE)/TASK 2/dual_port_ram_tb.sv
+++ AAT (CADENCE)/TASK 2/dual_port_ram_tb.sv
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // File        : dual_port_ram_tb.sv
-// Author      : Prajwal Bharadwaj D H (1BM23EC186)
+// Author      : Varsha R (1BM23EC284)
 // Created     : 2026-02-03
 // Module      : tb
 // Project     : SystemVerilog and Verification (23EC6PE2SV),
