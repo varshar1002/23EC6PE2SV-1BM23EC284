c9d08ae86d472c4877c4a5a83c9719cc793c6e27
varshar1002 <varshar.ec23@bmsce.ac.in>
2026-02-10 10:18:44 +0530

    Update and_gate.sv

--- EXP 1/and_gate.sv
+++ EXP 1/and_gate.sv
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // File        : and_gate.sv
-// Author      : Prajwal Bharadwaj D H (1BM23EC186)
+// Author      : Varsha R (1BM23EC284)
 // Created     : 21-01-2026
 // Module      : and_gate
 // Project     : SystemVerilog and Verification (23EC6PE2SV),
